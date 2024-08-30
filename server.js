75f0cfc4c9a9d7829c3fb0c5d232e69821ef0e65
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2024-08-30 22:28:16 +0530

    mongo

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const jsonServer = require("json-server"); // importing json-server library cyclic api
+const cors =require("cors")
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
+app.use(cors())
+
+const uri ="mongodb+srv://285rajkumarvishwakarma8759:DQtVbRjlYFXFvT6H@cluster0.isybh.mongodb.net/"
 server.use(middlewares);
 server.use(router);
 
