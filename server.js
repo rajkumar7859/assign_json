a605ae8c973fdbc135f1849ed40c28b834ec0b0a
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2024-09-01 12:40:06 +0530

    server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
-app.use(cors())
+server.use(cors())
 
 const uri ="mongodb+srv://285rajkumarvishwakarma8759:DQtVbRjlYFXFvT6H@cluster0.isybh.mongodb.net/"
 server.use(middlewares);
